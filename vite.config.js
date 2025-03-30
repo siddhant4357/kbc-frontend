ad186dde76ef0337db4cf0c4f841db2f050cd6d2
siddhant4357 <siddhantsankesara@gmail.com>
2025-03-30 22:22:49 +0530

    bhai finally done css

--- vite.config.js
+++ vite.config.js
@@ -1,55 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import purgecss from '@fullhuman/postcss-purgecss';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('autoprefixer'),
-        require('cssnano')({
-          preset: ['default', {
-            discardComments: { removeAll: true },
-            normalizeWhitespace: false
-          }]
-        }),
-        purgecss({
-          content: ['./src/**/*.{jsx,tsx,html}'],
-          safelist: [/^kbc-/, /^animate-/] // Safelist your important classes
-        })
-      ]
-    }
-  },
   build: {
     outDir: 'dist',
     sourcemap: false,
     cssCodeSplit: true,
-    cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          firebase: ['@firebase/app', '@firebase/database'],
-          styles: [
-            './src/styles/base.css',
-            './src/styles/animations.css',
-            './src/styles/components.css',
-            './src/styles/game.css',
-            './src/styles/responsive.css'
-          ]
-        },
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.css')) {
-            return 'assets/css/[name]-[hash][extname]';
-          }
-          return 'assets/[name]-[hash][extname]';
+          vendor: ['react', 'react-dom', 'react-router-dom', 'socket.io-client'],
+          firebase: ['@firebase/app', '@firebase/database']
         }
       }
     }
   },
   optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom', '@firebase/app', '@firebase/database']
+    include: [
+      'react', 
+      'react-dom', 
+      'react-router-dom', 
+      '@firebase/app', 
+      '@firebase/database',
+      'socket.io-client'
+    ]
   },
   resolve: {
     alias: {
