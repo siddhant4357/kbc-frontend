a84b83b49f62e9b98b9c2073476692012641c23c
siddhant4357 <siddhantsankesara@gmail.com>
2025-04-13 17:04:28 +0530

    huhh

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -1,18 +1,13 @@
-if (typeof process === 'undefined') {
+if (typeof window !== 'undefined' && !window.process) {
   window.process = {
     env: {},
     browser: true,
     version: '0.0.0',
     platform: 'browser',
-    nextTick: function(fn) { setTimeout(fn, 0); },
-    on: function() { return process; },
-    addListener: function() { return process; },
-    once: function() { return process; },
-    off: function() { return process; },
-    removeListener: function() { return process; },
-    removeAllListeners: function() { return process; },
-    emit: function() { return process; },
-    prependListener: function() { return process; },
-    prependOnceListener: function() { return process; }
+    nextTick: function(fn) { setTimeout(fn, 0); }
   };
+}
+
+if (typeof window !== 'undefined') {
+  window.global = window;
 }
--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,33 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
+    'process.env': '{}',
     'process.browser': true,
     'process.version': '"0.0.0"',
     'process.platform': '"browser"',
-    'process.on': '(() => {})',
     global: 'globalThis'
   },
   resolve: {
     alias: {
       '@': '/src',
-    },
+      'process': 'process/browser'
+    }
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis'
       }
+    }
+  },
+  server: {
+    watch: {
+      usePolling: true
     }
   }
 });
